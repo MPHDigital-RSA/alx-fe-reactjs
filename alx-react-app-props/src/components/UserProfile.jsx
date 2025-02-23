ea16b6b84ead5f81fbf9d88d2b4e2db4343cc4cf
Prince Hlela <mphdigital@protonmail.com>
2025-02-23 19:39:15 +0200

    my-webiste created using reactJS

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import UserContext from "./UserContext";
+import React, { useContext } from "react";
 
 function UserProfile() {
+    const { user } = useContext(UserContext);
     return (
         <div>
-
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
         </div>
-    )
+    );
 }
 
-export default UserProfile
+export default UserProfile;
