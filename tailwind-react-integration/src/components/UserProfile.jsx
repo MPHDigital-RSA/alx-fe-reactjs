c3c52660a678ab5f5200a5a11a700a7d23490c8f
Prince Hlela <mphdigital@protonmail.com>
2025-03-12 18:25:39 +0200

    created a project and installed tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 sm:max-width-xs max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 p-8">
+        <div className="user-profile bg-gray-100 sm:max-width-xs max-w-sx md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 p-8">
             <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" />
             <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
             <p className=" sm:text-sm text-gray-600 text-base md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
