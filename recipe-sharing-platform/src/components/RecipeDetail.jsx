4b11e79dc88544c880b2d218402044796d585297
Prince Hlela <mphdigital@protonmail.com>
2025-03-18 15:37:45 +0200

    done creating the homePage.jsx and styling the card component

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -16,9 +16,9 @@ function RecipeDetail() {
     return (
         <div className='w-[100%] min-h-[100vh] flex items-center justify-center px-[30px] flex-col gap-3 py-[50px]'>
 
-            <Link to='/' className='bg-[#ACBFA4] text-white rounded-lg py-[10px] px-[20px] hover:bg-black transition cursor-pointer'>Home</Link>
+            <Link to='/' className='bg-[#ACBFA4] text-white rounded-lg py-[10px] px-[20px] hover:bg-black transition cursor-pointer shadow'>Home</Link>
 
-            <div className='flex flex-col sm:flex-row gap-5 items-center bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left'>
+            <div className='flex flex-col sm:flex-row gap-5 items-center bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left shadow'>
                 <img src={currentRecipe.image} alt="" className='w-[100%] h-[300px] sm:h-[200px] sm:w-[50%] object-cover rounded-xl' />
                 <div>
                     <h1 className='text-[24px] sm:text-[28px] font-semibold'>{currentRecipe.title}</h1>
@@ -27,7 +27,7 @@ function RecipeDetail() {
             </div>
 
             <div className='grid gap-3 w-[100%] sm:grid-cols-2 w-[100%] max-w-[900px]'>
-                <div className='flex flex-col sm:flex-row gap-5 items-center bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left'>
+                <div className='flex flex-col sm:flex-row gap-5 items-center bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left shadow'>
                     <ul>
                         <h1 className='text-[24px] font-semibold mb-[10px]'>Ingredients</h1>
                         {currentRecipe.ingredients.map((ingredient) => {
@@ -38,7 +38,7 @@ function RecipeDetail() {
                     </ul>
                 </div>
 
-                <div className='flex flex-col bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left '>
+                <div className='flex flex-col bg-[#ACBFA4] p-[20px] rounded-xl text-white w-[100%] max-w-[900px] text-center sm:text-left shadow'>
                     <h1 className='text-[24px] font-semibold mb-[10px]'>Method</h1>
                     <p>{currentRecipe.instructions}</p>
                 </div>
