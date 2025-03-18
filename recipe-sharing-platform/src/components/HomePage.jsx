f2b9fbb7f7507166df4b293f91bc2f1e464612dc
Prince Hlela <mphdigital@protonmail.com>
2025-03-18 11:38:01 +0200

    done creating the homePage.jsx and styling the card component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import RecipeCard from './RecipeCard'
 import recipeData from '../data.json'
 
 function HomePage() {
 
-    const [data, setData] = useState(recipeData)
+    const [recipes, setRecipes] = useState(recipeData)
+
+    useEffect(() => {
+        console.log(recipes)
+    }, []);
 
     return (
-        <div className='grid gap-5 md:grid-cols-2 max-w-[900px] w-[100%] mx-[auto]'>
-            <RecipeCard index='0' data={data} />
-            <RecipeCard index='1' data={data} />
-        </div>
+        < div className='grid gap-5 md:grid-cols-2 max-w-[900px] w-[100%] mx-[auto]' >
+
+            {recipes.map((recipe) => {
+
+                return (
+                    <div className=" bg-[#ACBFA4] text-white flex px-[20px] py-[40px] items-center gap-[20px] flex-col rounded-xl hover:bg-[#B2BFAD] cursor-pointer shadow hover:shadow-xl" >
+                        <img src={recipe.image} alt="the image" className='w-[100px] h-[100px] object-cover rounded-xl' />
+                        <h1 className='text-[24px]'>{recipe.title}</h1>
+                        <p>{recipe.summary}</p>
+                    </div>
+                )
+            })}
+        </ div>
     )
 }
 
