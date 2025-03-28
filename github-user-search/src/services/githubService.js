4c98b74525b0b39170119e6d43eca29c075d7da8
Prince Hlela <mphdigital@protonmail.com>
2025-03-28 18:37:55 +0200

    Search.jsx created and the form input collected

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useGithubStore from '../stores/useGithubStore'
 
 const fetchUserData = async (user) => {
-    axios(`https://api.github.com/users/${user}`)
+    axios.get(`https://api.github.com/users/${user}`)
     .then((res) => {
         console.log(res.data)
     })
